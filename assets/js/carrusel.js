9cf60a54647d26695991cba4a8fbae0f0444fad8
sxgquimica <100764475+sxgquimica@users.noreply.github.com>
2024-11-18 20:07:10 +1100

    Update carrusel.js

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -20,7 +20,7 @@ function updateSlides(slideshowId) {
     
     // Muestra las 3 imágenes correspondientes
     for (let i = start; i < end; i++) {
-        const index = (i % totalSlides); // El operador módulo crea un bucle continuo
+        const index = i % totalSlides; // El operador módulo crea un bucle continuo
         slides[index].style.display = 'block';
     }
 }
@@ -31,12 +31,18 @@ function moveSlides(direction, slideshowId) {
     const totalSlides = slides.length;
 
     // Actualiza el índice actual según la dirección
-    currentIndex[slideshowId] += direction;
+    if (direction === 1) {
+        // Movimiento hacia la derecha: avanzar una posición
+        currentIndex[slideshowId]++;
+    } else if (direction === -1) {
+        // Movimiento hacia la izquierda: retroceder una posición
+        currentIndex[slideshowId]--;
+    }
 
     // Usamos el operador módulo para hacer que el carrusel sea cíclico
     if (currentIndex[slideshowId] < 0) {
-        currentIndex[slideshowId] = totalSlides - 1; // Cuando llega al principio, vuelve al final
-    } else if (currentIndex[slideshowId] >= totalSlides) {
+        currentIndex[slideshowId] = totalSlides - 3; // Cuando llega al principio, vuelve al final
+    } else if (currentIndex[slideshowId] + 3 > totalSlides) {
         currentIndex[slideshowId] = 0; // Cuando llega al final, vuelve al principio
     }
 
