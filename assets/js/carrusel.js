7938cbdffc67ea142742db14d97f35f2c1f7307f
sxgquimica <100764475+sxgquimica@users.noreply.github.com>
2024-11-18 19:28:00 +1100

    Update carrusel.js

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -1,5 +1,3 @@
-  // Carrusel.js
-
 // Crea un objeto para almacenar el índice actual de cada carrusel
 let currentIndex = {
     'slides1': 0,
@@ -18,7 +16,7 @@ function updateSlides(slideshowId) {
     // Oculta todas las imágenes
     slides.forEach(slide => slide.style.display = 'none');
     
-    // Muestra las 3 imágenes correspondientes
+    // Muestra las 3 imágenes correspondientes (en modo bucle)
     for (let i = start; i < end; i++) {
         const index = i % totalSlides; // Aquí usamos el operador módulo para crear el bucle
         slides[index].style.display = 'block';
@@ -33,11 +31,11 @@ function moveSlides(direction, slideshowId) {
     // Actualiza el índice actual según la dirección
     currentIndex[slideshowId] += direction;
 
-    // Usamos el operador módulo para crear el bucle infinito
+    // Si hemos llegado a la última imagen, volvemos al principio (bucle infinito)
     if (currentIndex[slideshowId] < 0) {
-        currentIndex[slideshowId] = totalSlides - 3;
+        currentIndex[slideshowId] = totalSlides - 3; // Mostrar las últimas 3 imágenes
     } else if (currentIndex[slideshowId] + 3 > totalSlides) {
-        currentIndex[slideshowId] = 0;
+        currentIndex[slideshowId] = 0; // Volver a la primera imagen
     }
 
     updateSlides(slideshowId);
