4fdcadfd17c206ac9600360e718e02893e58381c
sxgquimica <100764475+sxgquimica@users.noreply.github.com>
2024-11-18 17:50:51 +1100

    Update carrusel.js

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -4,15 +4,17 @@ function moveSlides(direction, slidesId) {
     const slides = document.getElementById(slidesId);
     const totalSlides = slides.children.length;
 
-    currentSlide += direction;
+       currentSlide += direction;
 
-   // Controlar el desplazamiento para que se mantenga dentro de los límites
+    // Si el desplazamiento es hacia atrás
     if (currentSlide < 0) {
-        currentSlide = totalSlides - 3;  // Volver al final si estamos al principio
-    } else if (currentSlide >= totalSlides) {
-        currentSlide = 0;  // Volver al principio si estamos al final
+        currentSlide = totalSlides - 3; // Vuelve al final si estamos al principio
+    } 
+    // Si el desplazamiento es hacia adelante
+    else if (currentSlide >= totalSlides - 2) { // 'totalSlides - 2' porque mostramos 3 imágenes
+        currentSlide = 0; // Vuelve al principio si estamos al final
     }
-    
+
     // Mueve el carrusel solo una imagen a la vez
-    slides.style.transform = `translateX(-${currentSlide * 33.3}%)`; // 33.3% por imagen
+    slides.style.transform = `translateX(-${currentSlide * 33.3}%)`; // 33.3% por cada imagen
 }
